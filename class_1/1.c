accd84f99fa632a392b1a4eda84f80ccb4220f86
mancanfly-1 <zqzq53373931@163.com>
2025-10-29 10:24:16 +0800

    output string

--- class_1/1.c
+++ class_1/1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	printf("helloworld");
-	printf("there is a update");
-	printf("helloworld");
+	// 字符串的输入和输出
+	char str_hel[] ="hello world";
+	printf("%s\n", str_hel);
+	printf("helloworld\n");
+	printf("%s\n", "helloworld");
 	return 0;
 }
